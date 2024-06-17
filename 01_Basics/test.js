4adad253711e27cf3adf503573bb2ab0e50f1b29
Prabin Kumar Sahu <78588455+Prabin-Kumar-Sahu@users.noreply.github.com>
2024-06-17 10:02:32 +0000

    variable

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,2 @@
 console.log("Prabin")
+console.log("sahu")
---
+++ 01_Basics/variable.js
@@ -0,0 +1,9 @@
+const accid=1234
+let accmail="Prabin@gmail"
+var accpas="12121212"
+acccity="BBSR"
+// accid=64
+accmail="papun@"
+accpas="123456"
+acccity="Cuttack"
+console.table([accid,accmail,accpas,acccity])
