ec1123ea97c0d9f5055c311accaa9899a352ae51
Prabin Kumar Sahu <78588455+Prabin-Kumar-Sahu@users.noreply.github.com>
2024-07-11 06:02:49 +0000

    asyn await

--- Advance/promise.js
+++ Advance/promise.js
@@ -37,23 +37,78 @@
 //     console.log(user)
 // })
 
-const promisefour = new Promise(function (resolve, reject) {
-  setTimeout(() => {
-    let error = true;
-    console.log("hello Promisefour complete");
-    if (!error) resolve({ username: "Prabin Kumar Sahu", PhoneNum: 938797213 });
-    reject("Error:Something went wrong");
-  }, 1000);
+// const promisefour = new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     let error = true;
+//     console.log("hello Promisefour complete");
+//     if (!error) resolve({ username: "Prabin Kumar Sahu", PhoneNum: 938797213 });
+//     reject("Error:Something went wrong");
+//   }, 1000);
+// })
+//   .then((user) => {
+//     console.log(user);
+//     // console.log(user.PhoneNum)
+//     // return user.PhoneNum;
+//     return user.username;
+//   })
+//   .then((user) => {
+//     console.log(user);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+// const promisefive = new Promise(function(resolve , reject)
+// {
+//   setTimeout(function()
+//   {
+//     let error = true
+//     console.log("This Promisefive either resolve or rejected ")
+//     if(!error)  {resolve({username : 'javascript',  password :1234})}
+//     reject('Error : javascript went wrong' )
+
+//   },1000)
+// })
+
+// async function consumepromisefive()
+// {
+//   try {
+//     const response = await promisefive
+//   console.log(response)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+// consumepromisefive()
+
+
+
+// async function getuserdetails()
+// {
+//     const response = await fetch("https://api.github.com/users/Prabin-Kumar-Sahu")
+//   try {
+//     const data =await response.json()
+//     console.log(data)
+//   } 
+// catch(error)
+// {
+//   console.log("E: ", error);
+// }
+  // const data =response.json()
+  //   console.log(data)
+
+// }
+// getuserdetails()
+
+
+fetch("https://api.github.com/users/Prabin-Kumar-Sahu").then((response)=>
+{
+  return response.json()
+
+}).then((data)=>
+{
+  console.log(data)
+}).catch((error)=>{
+console.log('Error :' , error)
 })
-  .then((user) => {
-    console.log(user);
-    // console.log(user.PhoneNum)
-    // return user.PhoneNum;
-    return user.username;
-  })
-  .then((user) => {
-    console.log(user);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
